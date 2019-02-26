e79217da7d1f40b3ceddb49cce1207f371df3d3f
angelomarcelino <amc.marcelino.cordeiro@gmail.com>
2019-02-26 12:51:04 -0300

    trocaregioes finalizado

--- atividades/trocaregioes.cpp
+++ atividades/trocaregioes.cpp
@@ -6,13 +6,35 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    Mat image;
-    Vec3b val;
+    Mat image, A, B, C, D, Vab, Vcd;
+    Rect rect;
 
-    image = imread(argv[1], CV_LOAD_IMAGE_COLOR );
+    image = imread(argv[1], CV_LOAD_IMAGE_COLOR);
     if (!image.data)
+    {
         cout << "A imagem nao foi carregada corretamente" << endl;
-
+        return -1;
+    }
     namedWindow("janela", WINDOW_AUTOSIZE);
+
+    imshow("janela", image);
+    cout << image.rows << " " << image.cols << endl;
+    waitKey();
     
+    rect = Rect(Point(0,0), Point(image.rows/2, image.cols/2));
+    A = Mat(image,rect);
+    rect = Rect(Point(0,image.cols/2), Point(image.rows/2, image.cols));
+    B = Mat(image,rect);
+    rect = Rect(Point(image.rows/2,0), Point(image.rows, image.cols/2));
+    C = Mat(image,rect);
+    rect = Rect(Point(image.rows/2, image.cols/2), Point(image.rows, image.cols));
+    D = Mat(image,rect);
+
+    vconcat(B, A, Vab);
+    vconcat(D, C, Vcd);
+    hconcat(Vcd, Vab, image);
+    imshow("janela", image);
+    waitKey();
+    
+    return 0;
 }
